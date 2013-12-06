8f8a79083a38bb2e7ced55c442cfc19477f5dbb8
kyonz <trevorjpreston@gmail.com>
2013-12-07 00:12:35 +1300

    Added logic to handle local whitelist bypass to Google from EurekaSettings

--- Whitelist-CGI.c
+++ Whitelist-CGI.c
@@ -6,12 +6,26 @@
 #include "unistd.h"
 
 int main(void) {
-  //Check for directory structure in Data (note using mkdir over stat checks for speed considerations)
-  //Now that www is in system, it will be created by imager.sh, but we still need our download folder
+  FILE *fp;
   mkdir("/data/eureka", 0777);
+  char path[1035];
 
-  //Print required header information
-  //Note: Date is currently static however no impact on CC
+  //Check if Whitelist bypass to Google is enabled
+  fp = popen("EurekaSettings get WhiteList useGoogle", "r");
+  //error out if command fails
+  if (fp == NULL) {
+  printf("Failed to run command\n" );
+  return 0;
+  }
+  //compare value of EurekaSettings
+  while (fgets(path, sizeof(path)-1, fp) != NULL) {
+  	if(compStr(path, "1")){
+		//Bypass enabled, redirect to Google
+  		printf( "HTTP/1.1 302 Object moved\n" );
+  		printf( "Location: http://clients3.google.com/cast/chromecast/device/baseconfig?b=14651\n\n\n" );
+	} else {
+
+  //Redirect not present, present local whitelist
   printf( "HTTP/1.1 200 OK\n" );
   printf( "Content-Type: application/json; charset=utf-8\n" );
   printf( "Cache-Control: no-cache, no-store, max-age=0, must-revalidate\n" );
@@ -27,7 +41,7 @@ int main(void) {
   printf( "Transfer-Encoding: chunked\n\n" );
 
   //Add configuration detail to whitelist
-  printf( ")]}'\n{\"configuration\":{\"idle_screen_app\":\"00000000-0000-0000-0000-000000000000\"},\"applications\":\n" );
+  printf( ")]}'\n{\"configuration\":{\"idle_screen_app\":\"00000000-0000-0000-0000-000000000000\"}, \"enabled_app_ids\": \"enabled_app_ids\":[\"087FCD5C\",\"2EEAFD9A\",\"30F4C306\",\"514E28B7\",\"552553BC\",\"674A0243\",\"6EBBD6E0\",\"85F0B427\",\"B88E20BD\",\"BFEBD3F1\",\"C27C2913\",\"C8939A18\",\"CC1AD845\",\"D6045317\",\"D8D09EE8\",\"E1F15514\",\"E9250490\",\"E93E3FCD\",\"EA126165\",\"ECE66B88\",\"FEE7AE75\"],\"applications\":\n" );
 
   //Read whitelist from file, print to web.
   FILE *ptr_file;
@@ -47,6 +61,27 @@ int main(void) {
   //Close whitelist configuration
   printf( "}" );
 
-  //Return successfully.
+	}
+
+  }
+  pclose(fp);
   return 0;
+
+}
+
+//Function to compare 2 character arrays for string comparison
+int compStr (const char *s1, char *s2, size_t sz) {
+    while (sz != 0) {
+        // At end of both strings, equal.
+        if ((*s1 == '\0') && (*s2 == '\0')) break;
+
+        // Treat spaces at end and end-string the same.
+        if ((*s1 == '\0') && (*s2 == ' ')) { s2++; sz--; continue; }
+        if ((*s1 == ' ') && (*s2 == '\0')) { s1++; sz--; continue; }
+
+        // Detect difference otherwise.
+        if (*s1 != *s2) return 0;
+        s1++; s2++; sz--;
+    }
+    return 1;
 }
